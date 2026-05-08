db5a3960060b44d63b267047cf9030a1cdecbf28
faisalrihman199 <129194773+faisalrihman199@users.noreply.github.com>
2026-05-09 00:23:03 +0500

    Some social links are added

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
-import { Youtube, Instagram, Facebook, Twitter } from 'lucide-react';
+import { Youtube, Instagram, Facebook } from 'lucide-react';
 import { Link, useNavigate } from 'react-router-dom';
 import { LanguageSwitcher } from '@/components/LanguageSwitcher';
 import { useGlobalState } from '@/context/GlobalStateContext';
 import { CurrencySwitcher } from '@/components/CurrencySwitcher';
+
+const TikTokIcon = ({ className }: { className?: string }) => (
+  <svg 
+    className={className} 
+    viewBox="0 0 24 24" 
+    fill="currentColor" 
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M19.589 6.686a4.793 4.793 0 0 1-3.77-4.245V2h-3.445v13.672a2.896 2.896 0 0 1-5.201 1.743l-.002-.001.002.001a2.895 2.895 0 0 1 3.183-4.51v-3.5a6.329 6.329 0 0 0-5.394 10.692 6.33 6.33 0 0 0 10.857-4.424V8.687a8.182 8.182 0 0 0 4.773 1.526V6.79a4.831 4.831 0 0 1-1.003-.104z" />
+  </svg>
+);
 
 const Footer = () => {
   const navigate = useNavigate();
@@ -102,10 +112,10 @@ const Footer = () => {
             <h6 className="text-[14px] font-bold text-[#60e677] uppercase tracking-widest mb-8 border-b border-white/10 pb-2 inline-block">Follow Us</h6>
             <div className="flex items-center gap-6">
               {[
-                { Icon: Youtube, href: "https://youtube.com" },
-                { Icon: Instagram, href: "https://instagram.com" },
+                { Icon: Instagram, href: "https://www.instagram.com/madeinus_marketplace?igsh=MWc5cjY4NjBhMnI3cA%3D%3D&utm_source=qr" },
+                { Icon: TikTokIcon, href: "https://www.tiktok.com/@made.in.us.com?_r=1&_t=ZG-96CDba99RLN" },
                 { Icon: Facebook, href: "https://facebook.com" },
-                { Icon: Twitter, href: "https://twitter.com" }
+                { Icon: Youtube, href: "https://youtube.com" }
               ].map(({ Icon, href }, idx) => (
                 <a
                   key={idx}
@@ -119,6 +129,8 @@ const Footer = () => {
               ))}
             </div>
           </div>
+
+
         </div>
 
         {/* Bottom Bar */}
